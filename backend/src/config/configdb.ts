cb473bdf64370cdbf7c261eeab0bfef029b21c07
ThuHoiBao <tranthu270904@gmail.com>
2025-09-17 16:41:32 +0700

    update

--- backend/src/config/configdb.ts
+++ backend/src/config/configdb.ts
@@ -4,13 +4,13 @@ import { seedProducts } from "../seeders/product.seed";
 
 const connectDB = async (): Promise<void> => {
   try {
-    const mongoURI: string = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/DB_Futu";
+    const mongoURI: string = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/DB_02";
 
     // Phiên bản mới không cần useNewUrlParser và useUnifiedTopology
     await mongoose.connect(mongoURI);
 
     console.log("MongoDB connection established successfully.");
-    await seedProducts();
+    // await seedProducts();
   } catch (error: any) {  // để TypeScript không báo lỗi khi log error
     console.log("Unable to connect to MongoDB:", error.message || error);
   }
