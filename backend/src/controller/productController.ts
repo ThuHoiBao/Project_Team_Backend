f0f804f9058b8df5dabf1608133a2ba24719f816
MinhNguyenCR <minh.dox.555@gmail.com>
2025-09-30 17:04:21 +0700

    list favorite api

--- backend/src/controller/productController.ts
+++ backend/src/controller/productController.ts
@@ -1,9 +1,12 @@
-import { productDetailService, findProductByCategoryIdService, getSizebyProductIdService } from "../service/productService";
+import { log } from "node:console";
+import { productDetailService, findProductByCategoryIdService, getSizebyProductIdService, addToWWishlistService,
+  deleteFromWishlistService,checkProductExistedWishlistService, getWishlistService
+ } from "../service/productService";
 import { Request, Response } from "express";
 
 export const productDetail = async (req: Request, res: Response) => {
   const { id } = req.params;
-  
+
   try {
     const response = await productDetailService(id);
 
@@ -19,10 +22,10 @@ export const productDetail = async (req: Request, res: Response) => {
 
 
 export const findProductByCategoryId = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  
+  const { id, productId } = req.params;
+
   try {
-    const response = await findProductByCategoryIdService(id);
+    const response = await findProductByCategoryIdService(id, productId);
 
     if (!response.success) {
       return res.status(404).json(response);
@@ -36,8 +39,8 @@ export const findProductByCategoryId = async (req: Request, res: Response) => {
 
 
 export const getSizebyProductId = async (req: Request, res: Response) => {
-  const {id} = req.params;
-  try{
+  const { id } = req.params;
+  try {
     const response = await getSizebyProductIdService(id);
     if (!response.success) {
       return res.status(404).json(response);
@@ -48,3 +51,59 @@ export const getSizebyProductId = async (req: Request, res: Response) => {
     return res.status(500).json({ message: error.message });
   }
 }
+
+export const addToWWishlist = async (req, res) => {
+  const userId = req.user.id;
+  const productId = req.params.id
+  
+  try {
+    const response = await addToWWishlistService(userId, productId);
+    if (!response.success) {
+      return res.status(404).json(response);
+    }
+
+    return res.status(200).json(response);
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+export const deleteFromWishlist = async (req, res) => {
+  const userId = req.user.id;
+  const productId = req.params.id
+  
+  try {
+    const response = await deleteFromWishlistService(userId, productId);
+    if (!response.success) {
+      return res.status(404).json(response);
+    }
+
+    return res.status(200).json(response);
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+export const checkProductExistedWishlist = async (req, res) => {
+  const userId = req.user.id;
+  const productId = req.params.id;
+  try {
+    const response = await checkProductExistedWishlistService(userId, productId);
+    return res.status(200).json({
+      existed: response
+    });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+export const getWishlist = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const response = await getWishlistService(userId);
+    return res.status(200).json(response);
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
--- backend/src/route/productRoutes.ts
+++ backend/src/route/productRoutes.ts
@@ -1,13 +1,19 @@
 import express from 'express';
-
+import authenticateToken from '../middleware/authenticateToken.ts'; 
 import { getProductPagination } from '../controller/searchController.ts';
-import { productDetail,findProductByCategoryId, getSizebyProductId } from '../controller/productController.ts';
+import { productDetail,findProductByCategoryId, getSizebyProductId,addToWWishlist, deleteFromWishlist, 
+    checkProductExistedWishlist, getWishlist
+ } from '../controller/productController.ts';
 
 const router = express.Router();
 
 router.get('/product/search', getProductPagination );
+router.get('/product/wishlist', authenticateToken, getWishlist)
 router.get('/product/:id', productDetail);
-router.get('/product/category/:id',findProductByCategoryId);
+router.get('/product/category/:id/:productId',findProductByCategoryId);
 router.get('/product/size/:id', getSizebyProductId);
+router.post('/product/wishlist/add/:id', authenticateToken, addToWWishlist)
+router.delete('/product/wishlist/delete/:id', authenticateToken, deleteFromWishlist)
+router.get('/product/wishlist/existed/:id', authenticateToken, checkProductExistedWishlist)
 export default router;
  
